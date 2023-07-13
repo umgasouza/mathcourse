9d3ee476bccc0e09792162b229db97b5f39f7765
G. A. de Souza <136265154+umgasouza@users.noreply.github.com>
2023-07-13 12:33:00 +0000

    feat: Added calculate scalar product of two vectors

--- src/analyticgeo.cpp
+++ src/analyticgeo.cpp
@@ -24,7 +24,7 @@ void calc_vector() {
     vector.x = bx - ax;
     vector.y = by - ay;
 
-    fprintf(stdout, "X: %d | Y: %d\n", vector.x, vector.y);
+    fprintf(stdout, "X: %.2f | Y: %.2f\n", vector.x, vector.y);
 }
 
 void calc_vector_norm() {
@@ -34,6 +34,7 @@ void calc_vector_norm() {
     std::cin >> vector.x;
     fprintf(stdout, "Type the value for the y component: ");
     std::cin >> vector.y;
+
     float x_sq = pow(vector.x, 2);
     float y_sq = pow(vector.y, 2);
 
@@ -45,25 +46,64 @@ void calc_vector_norm() {
 	    vector_norm);
 }
 
+void calc_vector_by_scalar() {
+    // TODO
+}
+
+void calc_vector_scalar() {
+    Point2D vector_a, vector_b;
+
+    fprintf(stdout, "Type the value for the A.x component: ");
+    std::cin >> vector_a.x;
+    fprintf(stdout, "Type the value for the A.y component: ");
+    std::cin >> vector_a.y;
+
+    fprintf(stdout, "Type the value for the B.x component: ");
+    std::cin >> vector_b.x;
+    fprintf(stdout, "Type the value for the B.y component: ");
+    std::cin >> vector_b.y;
+
+    float value_one = vector_a.x * vector_b.x;
+    float value_two = vector_a.y * vector_b.y;
+    
+    float result = value_one + value_two;
+
+    fprintf(stdout, "The scalar product of (%.2f, %.2f) and (%.2f, %.2f) is: %.2f\n",
+	    vector_a.x,
+	    vector_a.y,
+	    vector_b.x,
+        vector_b.y,
+        result);
+}
+
+void calc_vector_cross_product() {
+    // TODO
+}
+
 int main() {
     int input = -1;
 
     while(input != 0) {
-	fprintf(stdout, "Menu:\n");
-	fprintf(stdout, "1: Calculate Vector\n");
-	fprintf(stdout, "2: Calculate Vector Norm\n");
-	fprintf(stdout, "0: Exit\n");
-	fprintf(stdout, "Choose: ");
-	std::cin >> input;
-	switch(input) {
-	    case 1:
-		calc_vector();
-		break;
-	    case 2:
-		calc_vector_norm();
-		break;
-	    default:
-		return 0;
-	}
+	    fprintf(stdout, "Menu:\n");
+	    fprintf(stdout, "1: Calculate Vector\n");
+	    fprintf(stdout, "2: Calculate Vector Norm\n");
+        fprintf(stdout, "3: Calculate the scalar product\n");
+	    fprintf(stdout, "0: Exit\n");
+	    fprintf(stdout, "Choose: ");
+	    std::cin >> input;
+	    switch(input) {
+	        case 1:
+		        calc_vector();
+		        break;
+	        case 2:
+		        calc_vector_norm();
+		        break;
+            case 3:
+                calc_vector_scalar();
+                break;
+	        default:
+		        break;
+	    }
     }
+    return 0;
 }
